774b03137eb9ca36641ea7e3625d3192bd785c98
Matheus Henrique Maia Ramos <matheus.henrimr@gmail.com>
2024-05-20 11:07:43 -0300

    feat. connection to FirestoreDatabase

--- app/src/main/java/versioning/compose/src/MainActivity.kt
+++ app/src/main/java/versioning/compose/src/MainActivity.kt
@@ -14,10 +14,12 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,26 +28,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.firebase.firestore.FirebaseFirestore
 import versioning.compose.src.ui.theme.LearningJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val database = FirebaseFirestore.getInstance()
             LearningJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(), // Fill the maximum size available
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AuthScreen(onEnterClick = {
-                        Log.i("MainActivity", "onCreate: $it")
-                    })
+                    Column(
+                        Modifier.padding(4.dp),
+                    ) {
+                        AuthScreen(onEnterClick = {
+                            Log.i("MainActivity", "onCreate: $it")
+                        })
+                        CustomSliderScreen(database)
+                    }
                 }
             }
         }
     }
 }
+
 
 /*
 * Os componentes tendem a ser stateless
@@ -54,8 +64,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AuthScreen(
-    onEnterClick: (User) -> Unit)
-{
+    onEnterClick: (User) -> Unit
+) {
     Column {
         var username by remember {
             mutableStateOf("")
@@ -128,6 +138,62 @@ fun AuthScreenPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             AuthScreen(onEnterClick = {})
+        }
+    }
+}
+
+@Composable
+fun CustomSliderScreen(database: FirebaseFirestore) {
+    var sliderValue by remember {
+        mutableStateOf(0f)
+    }
+    LaunchedEffect(sliderValue) {
+        val updates: Map<String, Any?> = hashMapOf(
+            "Ligado" to false, // Assuming you want to set Ligado to false initially
+            "Velocidade" to sliderValue.toInt()
+        )
+
+        // Reference to the document
+        val docRef = database.collection("Ventilador").document("2")
+
+        // Update the document with the new values
+        try {
+            docRef.update(updates).addOnSuccessListener {
+                Log.d("FirestoreUpdate", "Document successfully updated.")
+            }.addOnFailureListener { e ->
+                Log.e("FirestoreUpdate", "Error updating document.", e)
+            }
+        } catch (e: Exception) {
+            Log.e("FirestoreUpdate", "Exception occurred.", e)
+        }
+    }
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Slider(
+            modifier = Modifier.fillMaxWidth(),
+            value = sliderValue,
+            onValueChange = {
+                sliderValue = it
+                Log.i("MainActivity", "Slider: $it")
+            },
+            valueRange = 0f..255f,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SliderPreview() {
+    LearningJetpackComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            CustomSliderScreen(database = FirebaseFirestore.getInstance())
         }
     }
 }
