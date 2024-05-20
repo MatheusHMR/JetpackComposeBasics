c80a69385903ada4471af025b30bda2750f20690
Matheus Henrique Maia Ramos <matheus.henrimr@gmail.com>
2024-05-20 16:03:01 -0300

    feat. sliders and switchs for lamp and fan control

--- app/src/main/java/versioning/compose/src/MainActivity.kt
+++ app/src/main/java/versioning/compose/src/MainActivity.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,23 +14,30 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.protobuf.DescriptorProtos.FieldDescriptorProto.Label
 import versioning.compose.src.ui.theme.LearningJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,12 +52,13 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column(
-                        Modifier.padding(4.dp),
+                        Modifier.padding(16.dp),
                     ) {
-                        AuthScreen(onEnterClick = {
-                            Log.i("MainActivity", "onCreate: $it")
-                        })
-                        CustomSliderScreen(database)
+//                        AuthScreen(onEnterClick = {
+//                            Log.i("MainActivity", "onCreate: $it")
+//                        })
+                        LampadaOperations(database)
+                        VentiladorOperations(database)
                     }
                 }
             }
@@ -62,100 +72,202 @@ class MainActivity : ComponentActivity() {
 * Precisamos configurar estados para coisas que queremos alterar valores com base em eventos
 * */
 
+//@Composable
+//fun AuthScreen(
+//    onEnterClick: (User) -> Unit
+//) {
+//    Column {
+//        var username by remember {
+//            mutableStateOf("")
+//        }
+//        var password by remember {
+//            mutableStateOf("")
+//        }
+//        TextField(
+//            value = username,
+//            onValueChange = { newValue ->
+//                username = newValue
+//            },
+//            Modifier
+//                .padding(16.dp)
+//                .fillMaxWidth(),
+//            label = {
+//                Text("Usuário")
+//            },
+//            leadingIcon = {
+//                Icon(
+//                    imageVector = Icons.Default.AccountCircle,
+//                    contentDescription = "Ícone que representa o usuário"
+//                )
+//            }
+//        )
+//        TextField(
+//            value = password,
+//            onValueChange = {
+//                password = it
+//            },
+//            Modifier
+//                .padding(16.dp)
+//                .fillMaxWidth(),
+//            visualTransformation = PasswordVisualTransformation(),
+//            label = {
+//                Text("Senha")
+//            },
+//            leadingIcon = {
+//                Icon(
+//                    imageVector = Icons.Default.Lock,
+//                    contentDescription = "Ícone de representação de senha em forma de reticências"
+//                )
+//            }
+//        )
+//        Button(
+//            onClick = {
+//                onEnterClick(
+//                    User(
+//                        username,
+//                        password
+//                    )
+//                )
+//            },
+//            Modifier
+//                .padding(16.dp)
+//                .fillMaxWidth(),
+//        ) {
+//            Text(text = "Entrar")
+//        }
+//    }
+//}
+
+//@Preview
+//@Composable
+//fun AuthScreenPreview() {
+//    LearningJetpackComposeTheme {
+//        // A surface container using the 'background' color from the theme
+//        Surface(
+//            modifier = Modifier.fillMaxSize(),
+//            color = MaterialTheme.colorScheme.background
+//        ) {
+//            AuthScreen(onEnterClick = {})
+//        }
+//    }
+//}
+
 @Composable
-fun AuthScreen(
-    onEnterClick: (User) -> Unit
-) {
-    Column {
-        var username by remember {
-            mutableStateOf("")
-        }
-        var password by remember {
-            mutableStateOf("")
-        }
-        TextField(
-            value = username,
-            onValueChange = { newValue ->
-                username = newValue
-            },
-            Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            label = {
-                Text("Usuário")
-            },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.AccountCircle,
-                    contentDescription = "Ícone que representa o usuário"
-                )
+fun LampadaOperations(database: FirebaseFirestore) {
+    val docRef = database.collection("Lâmpada").document("1")
+    var stateButton by remember { mutableStateOf(false)}
+
+    DisposableEffect(Unit) {
+        val listenerRegistration = docRef.addSnapshotListener { snapshot, error ->
+            if(error != null){
+                Log.e("FirestoreSnapshot", "Error listening to changes", error)
+                return@addSnapshotListener
             }
-        )
-        TextField(
-            value = password,
-            onValueChange = {
-                password = it
-            },
-            Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            visualTransformation = PasswordVisualTransformation(),
-            label = {
-                Text("Senha")
-            },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Lock,
-                    contentDescription = "Ícone de representação de senha em forma de reticências"
-                )
+
+            if (snapshot != null && snapshot.exists()) {
+                stateButton = snapshot.getBoolean("Ligado") ?: false
             }
-        )
-        Button(
-            onClick = {
-                onEnterClick(
-                    User(
-                        username,
-                        password
-                    )
-                )
-            },
-            Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-        ) {
-            Text(text = "Entrar")
         }
+        onDispose { listenerRegistration.remove() }
+    }
+
+    Row (
+        Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(8.dp)
+                .weight(6f),
+            text = "Controle a Lâmpada",
+        )
+        Switch(checked = stateButton,
+            onCheckedChange = { newState ->
+                stateButton = newState
+                docRef.update("Ligado", newState)
+                    .addOnSuccessListener {
+                        Log.d("FirestoreUpdate", "Switch successfully updated.")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e("FirestoreUpdate", "Error updating switch.", e)
+                    }
+            })
     }
 }
 
-@Preview
 @Composable
-fun AuthScreenPreview() {
-    LearningJetpackComposeTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            AuthScreen(onEnterClick = {})
-        }
+fun VentiladorOperations(database: FirebaseFirestore) {
+    val collectionRef = database.collection("Ventilador")
+
+    Column(
+        Modifier
+            .fillMaxSize()
+//            .padding(horizontal = 64.dp, vertical = 64.dp)
+//            .padding(16.dp)
+    ) {
+        CustomSliderScreen(collectionRef)
+        SwitchButtonVentilador(collectionRef)
     }
 }
 
 @Composable
-fun CustomSliderScreen(database: FirebaseFirestore) {
-    var sliderValue by remember {
-        mutableStateOf(0f)
+fun SwitchButtonVentilador(collectionRef: CollectionReference) {
+    val docRef = collectionRef.document("2")
+    var stateButton by remember { mutableStateOf(false) }
+
+    DisposableEffect(Unit) {
+        val listenerRegistration = docRef.addSnapshotListener { snapshot, error ->
+            if (error != null) {
+                Log.e("FirestoreSnapshot", "Error listening to changes", error)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                stateButton = snapshot.getBoolean("Ligado") ?: false
+            }
+        }
+
+        onDispose { listenerRegistration.remove() }
     }
+
+    Row(
+        Modifier.fillMaxWidth()
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(8.dp)
+                .weight(6f),
+            text = "Controle o Ventilador",
+        )
+        Switch(
+            checked = stateButton,
+            onCheckedChange = { newState ->
+                stateButton = newState
+                docRef.update("Ligado", newState)
+                    .addOnSuccessListener {
+                        Log.d("FirestoreUpdate", "Switch successfully updated.")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e("FirestoreUpdate", "Error updating switch.", e)
+                    }
+            }
+        )
+    }
+}
+
+@Composable
+fun CustomSliderScreen(collectionRef: CollectionReference) {
+
+    var sliderValue by remember { mutableStateOf(0f) }
+    val docRef = collectionRef.document("2")
+
     LaunchedEffect(sliderValue) {
         val updates: Map<String, Any?> = hashMapOf(
             "Ligado" to false, // Assuming you want to set Ligado to false initially
             "Velocidade" to sliderValue.toInt()
         )
-
-        // Reference to the document
-        val docRef = database.collection("Ventilador").document("2")
-
         // Update the document with the new values
         try {
             docRef.update(updates).addOnSuccessListener {
@@ -167,21 +279,18 @@ fun CustomSliderScreen(database: FirebaseFirestore) {
             Log.e("FirestoreUpdate", "Exception occurred.", e)
         }
     }
-    Column(
-        Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Slider(
-            modifier = Modifier.fillMaxWidth(),
-            value = sliderValue,
-            onValueChange = {
-                sliderValue = it
-                Log.i("MainActivity", "Slider: $it")
-            },
-            valueRange = 0f..255f,
-        )
-    }
+    Slider(
+        modifier =
+        Modifier.fillMaxWidth(),
+//                .rotate(270f),
+        value = sliderValue,
+        onValueChange = {
+            sliderValue = it
+            Log.i("MainActivity", "Slider: $it")
+        },
+        valueRange = 0f..255f,
+    )
+
 }
 
 @Preview
@@ -193,7 +302,6 @@ fun SliderPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            CustomSliderScreen(database = FirebaseFirestore.getInstance())
         }
     }
 }
